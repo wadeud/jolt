57f1184106f98ee3e4e8b15b8c27f3e70bd70231
wadeud <161763388+wadeud@users.noreply.github.com>
2024-08-18 07:32:06 +0100

    Update iai.rs

--- jolt-core/benches/iai.rs
+++ jolt-core/benches/iai.rs
@@ -5,16 +5,17 @@ use iai_callgrind::{library_benchmark, library_benchmark_group, main};
 use jolt_core::{field::JoltField, msm::VariableBaseMSM, poly::dense_mlpoly::DensePolynomial};
 use std::hint::black_box;
 
+/// Setup function to generate random points and scalars for MSM benchmarking.
 fn msm_setup<G: CurveGroup>(num_points: usize) -> (Vec<G>, Vec<G::ScalarField>) {
     let mut rng = test_rng();
 
-    // Generate a vector of random affine points on the curve.
     (
         vec![G::rand(&mut rng); num_points],
         vec![G::ScalarField::rand(&mut rng); num_points],
     )
 }
 
+/// Setup function to generate a polynomial and a bound value for polynomial binding benchmarking.
 fn bound_poly_setup<F: JoltField>(size: usize) -> (DensePolynomial<F>, F) {
     let mut rng = test_rng();
 
@@ -24,6 +25,7 @@ fn bound_poly_setup<F: JoltField>(size: usize) -> (DensePolynomial<F>, F) {
     )
 }
 
+/// Setup function to generate a polynomial and a set of points for polynomial evaluation benchmarking.
 fn eval_poly_setup<F: JoltField>(size: usize) -> (DensePolynomial<F>, Vec<F>) {
     let mut rng = test_rng();
 
@@ -32,12 +34,14 @@ fn eval_poly_setup<F: JoltField>(size: usize) -> (DensePolynomial<F>, Vec<F>) {
     (poly, points)
 }
 
+/// Benchmark function for Multi-Scalar Multiplication (MSM) using variable-base MSM.
 #[library_benchmark]
 #[bench::long(msm_setup::<G1Projective>(4096))]
 fn bench_msm<G: CurveGroup>(input: (Vec<G>, Vec<G::ScalarField>)) -> G {
     black_box(VariableBaseMSM::msm(&G::normalize_batch(&input.0), &input.1).unwrap())
 }
 
+/// Benchmark function for polynomial binding with a bound value.
 #[library_benchmark]
 #[bench::long(bound_poly_setup::<Fr>(4096))]
 fn bench_polynomial_binding<F: JoltField>(input: (DensePolynomial<F>, F)) {
@@ -45,6 +49,7 @@ fn bench_polynomial_binding<F: JoltField>(input: (DensePolynomial<F>, F)) {
     poly.bound_poly_var_top(&val);
 }
 
+/// Benchmark function for polynomial evaluation at a set of points.
 #[library_benchmark]
 #[bench::long(eval_poly_setup::<Fr>(4096))]
 fn bench_polynomial_evaluate<F: JoltField>(input: (DensePolynomial<F>, Vec<F>)) -> F {
